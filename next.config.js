a7c7a4d3c3068074ba69a467c4f9dec8ccddaaa5
greene5952 <dustin@wolf-solutions.us>
2022-07-18 22:30:06 -0500

    Initial

---
+++ next.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  //reactStrictMode: false,
+  //generateEtags: false,
+  reactStrictMode: false,
+  analyticsId: '7vsHWbYwcKpA3lFTng2Qe9iNzE7',
+  env: {
+    siteTitle: "Stuffy Bunny NFT Minting Dapp",
+    contractAddress: "0xB621fdFDda960D9007705cE6e56Afa7b8dDF26E4",
+    infuraID: "045e9c65387a4200aa8559c347fa19a4",
+    defaultGas: 125000,//75000,
+    mintType : "Pre-Sale", //Pre-Sale Public
+    enforceWhitelist : true,
+    ethValue : "0.01",
+    ethWLValue : "0.00",
+    debug: false,
+    network: 'mainnet',
+    chainId: 1,
+    pageHeader : 'NFTs for real world use',
+    pageText : 'Blending the power of NFTs and real life business. We are lowering the barrier to entry into the NFT space, making this a perfect first time project for anyone',
+    //mintType: "Whitelist",
+    blockExplorerURL: 'https://etherscan.io/',
+    www: 'https://stuffybunnynft.com/',
+    mainWWW: 'https://stuffybunnynft.com/',
+    title: 'Stuffy Bunny NFT',
+    links : {
+      opensea : "https://opensea.io/collection/stuffy-bunny-nft",
+      discord : "https://discord.com/invite/stuffybunny",
+      twitter : "https://twitter.com/OfficialSB_NFT"
+    },
+    maxMint: 5000,
+    maxMintCount: 100,
+    messagehash: '0x865f293da4a629f00bd8072be6115031c608fa4a13b819fd087811752fc8bb90',
+    signatureBytes: '0x4b7715f34823936b045d1e5ca6c295b7f5ecff3d0c158118c71441ed217c3ddb026617ce49dc15781bdb25c8c34ed4a1f4d68df6080f6dd0004511cf05032bf01c',
+    messagehash2: '0x417ebaa8de17c9a73acf7cf3f6c998ca2dd1e993276cc9dd37dfb7fb0cb197a4',
+    signatureBytes2: '0xe191166afd511a08818c2f55906e65db02dfe2366708885e92a2e54cef7ac2673e5dfe70c2d0d63a337c7792906a623f3aff0506aa2c5de6325e2d689bab95731b'
+  },
+  images: {
+    domains: ['static.wixstatic.com']
+  }
+}
