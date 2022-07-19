29c96d2c2887e70e58047daaa936806adc82c372
greene5952 <dustin@wolf-solutions.us>
2022-07-19 13:32:14 -0500

    yuk

--- layout/header/Header.js
+++ layout/header/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
                     <a
                       className={
                         router.pathname == "/"
-                          ? "text-white nav-link"
+                          ? "text-pink nav-link"
                           : "nav-link"
                       }
                     >
